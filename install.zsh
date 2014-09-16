7d4c010d941752b4d864c2398ad3e5ae1da6c9b7
Adam Jurczyk <adam.jurczyk@grupaonet.pl>
2014-09-17 01:48:21 +0200

    renames and install script

---
+++ install.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+echo "-- install/update prezto"
+
+if [[ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]]; then
+	git clone --recursive https://github.com/ajur/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+elif; then
+	cd "${ZDOTDIR:-$HOME}/.zprezto"
+	git pull origin master
+fi
+
+echo "-- link dotfiles"
+
+setopt EXTENDED_GLOB
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+ 	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+for rcfile in $HOME/.dotfiles/^(README.md|install.zsh); do
+	ln -s "$rcfile" "$HOME/.${rcfile:t}"
+done

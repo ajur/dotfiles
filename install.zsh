422994e299a0b8c6dfa6d2444d9fe366d04e7e9d
Adam Jurczyk <adam.jurczyk@grupaonet.pl>
2014-09-17 22:18:22 +0200

    reset tmux action key

--- install.zsh
+++ install.zsh
@@ -20,7 +20,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-for rcfile in $HOME/.dotfiles/^(README.md|install.zsh); do
+for rcfile in $HOME/.dotfiles/^(README.md|install.zsh|.*); do
     if [[ -e "${ZDOTDIR:-$HOME}/.${rcfile:t}" ]]; then
         rm -rf "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     fi
